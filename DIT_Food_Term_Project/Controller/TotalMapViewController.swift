027793df0cfd2b758aa86b42463caab4d4278b81
D7703_22 <d7703_22@D7703-22ui-iMac-2.local>
2018-06-29 10:15:00 +0900

    totalMapView connect

---
+++ DIT_Food_Term_Project/Controller/TotalMapViewController.swift
@@ -0,0 +1,65 @@
+//
+//  TotalMapViewController.swift
+//  DIT_Food_Term_Project
+//
+//  Created by D7703_22 on 2018. 6. 29..
+//  Copyright © 2018년 s22dfg. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TotalMapViewController: UIViewController {
+
+    @IBOutlet weak var totalMapView: MKMapView!
+    
+    var location = ""
+    var name = ""
+    var tel = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(location, completionHandler: {
+            (placemarks: [CLPlacemark]?, error: Error?) -> Void in
+            
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            if placemarks != nil {
+                let placemark = placemarks![0]
+                
+                // Add annotation
+                let annotation = MKPointAnnotation()
+                annotation.title = self.name
+                annotation.subtitle = self.tel
+                
+                if let location = placemark.location {
+                    annotation.coordinate = location.coordinate
+                    self.totalMapView.addAnnotation(annotation)
+                    
+                    // Set zoom level
+                    let region = MKCoordinateRegionMakeWithDistance(annotation.coordinate, 700, 700)
+                    self.totalMapView.setRegion(region, animated: true)
+                    self.totalMapView.selectAnnotation(annotation, animated: true)
+                }
+            }
+        })
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    }
+    
+}
+
