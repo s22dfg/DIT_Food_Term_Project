f648f8cd7dbc449e2cc97b9599c86b3cff165b08
D7703_22 <d7703_22@D7703-22ui-iMac-2.local>
2018-06-29 10:06:26 +0900

    connect table view controller

---
+++ DIT_Food_Term_Project/MapViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MapViewController.swift
+//  DIT_Food_Term_Project
+//
+//  Created by D7703_22 on 2018. 6. 29..
+//  Copyright © 2018년 s22dfg. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
