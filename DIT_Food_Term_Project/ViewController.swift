9ed62be4d3472d27f217ea98e9a671a1af958e84
D7703_22 <d7703_22@D7703-22ui-iMac-2.local>
2018-06-29 09:40:59 +0900

    Initial Commit

---
+++ DIT_Food_Term_Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DIT_Food_Term_Project
+//
+//  Created by D7703_22 on 2018. 6. 29..
+//  Copyright © 2018년 s22dfg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
